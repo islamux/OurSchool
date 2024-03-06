f7938bb6716cc18166dc43caf2fcf418dbcdc8e7
islamux <fathi733@gmail.com>
2024-03-06 11:27:34 +0300

    delete unnecessary comments

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -14,10 +14,8 @@ public class  Gui {
        PrintSchoolWelcomeGui printSchoolWelcomeGui = new PrintSchoolWelcomeGui();
        MainMenuFuncGui mainMenuFuncGui = new MainMenuFuncGui();
 
-        School school = setSchoolWelcomeGui.setSchoolWelcomeInfo(); // حسب مافهمت تم انشاء اوبجكت لمرة واحده ثم يتم تعبئته عند بد التطبيق وطالما واللوب يعمل ولم يخرج البرنامج
-        SchoolServices schoolServices = new SchoolServices(school); // السكول سرفسس يتم تعبئتها عند بدا البرنامج وتستخدم في كل البرنامج وطالما ان اللوب يعمل  ^_^
-        // حفظك الله ياباش مهندس محمد ورحم والديك
-        // سيتم تمريرها الى جميع الفنكشنات المنفصله بحمد الله ومنه وفضله
+        School school = setSchoolWelcomeGui.setSchoolWelcomeInfo(); 
+        SchoolServices schoolServices = new SchoolServices(school); 
         printSchoolWelcomeGui.printSchoolWelcomeInfo(school);
         mainMenuFuncGui.mainMenu(schoolServices);
 
